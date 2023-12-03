eefdf82adcd3fd9f37593fe831647afb4d696671
dhershman1 <dustinh17@gmail.com>
2023-12-03 01:17:17 -0500

    Updated: Format param for getting data

--- utils/getData.js
+++ utils/getData.js
@@ -1,12 +1,16 @@
 import fs from 'node:fs/promises'
 import { EOL } from 'node:os'
 
-async function getData () {
+async function getData (format = true) {
   const data = await fs.readFile('./data.txt', {
     encoding: 'utf-8'
   })
 
-  return data.split(EOL).filter(s => s)
+  if (format) {
+    return data.split(EOL).filter(s => s)
+  }
+
+  return data
 }
 
 export default getData
